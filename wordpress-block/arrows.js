f5c042f3cbd966f9ef39b42b4a178f42ab1130d6
Ferran <ferranllop@gmail.com>
2020-09-25 18:04:33 +0200

    modify css and finalize wordpress block

---
+++ wordpress-block/arrows.js
@@ -0,0 +1,152 @@
+(function () {
+    const { __ } = wp.i18n
+    const { registerBlockType, getBlockDefaultClassName } = wp.blocks;
+    const { InspectorControls } = wp.blockEditor;
+    const { RangeControl, TextControl } = wp.components;
+    const { createElement: el } = wp.element;
+    const renderArrow = (attributes) => {
+        return (
+            el('div', {
+                className: 'arrowscss-wrapper'
+            }, [
+                el('div', {
+                    className: 'arrowscss',
+                    'data-start': attributes.arrowFrom,
+                    'data-slope': attributes.arrowSlope,
+                    'data-curve-type': attributes.arrowCurve,
+                    style: {
+                        top: attributes.wrapperTop + '%',
+                        left: attributes.wrapperLeft + '%',
+                        height: attributes.wrapperHeight + '%',
+                        width: attributes.wrapperWidth + '%',
+                        '--cap-size': attributes.capSize + 'em',
+                        '--stroke': attributes.stroke + 'em'
+                    }
+                }, [
+                    el('div', { className: 'cap', 'data-position': attributes.capPosition })
+                ])
+            ])
+        )
+    }
+
+    registerBlockType('djmarian/arrows', {
+        title: __('Arrows', 'djmarian'),
+        description: __('Render arrows', 'djmarian'),
+        icon: 'arrow-down-alt',
+        category: 'widgets',
+        // parent: ['core/column'],
+        attributes: {
+            wrapperTop: { default: 0 },
+            wrapperLeft: { default: 0 },
+            wrapperHeight: { default: 100 },
+            wrapperWidth: { default: 100 },
+            capSize: { default: 4 },
+            stroke: { default: 0.7 },
+            arrowFrom: { default: 'top-left' },
+            arrowSlope: { default: 'horizontal' },
+            arrowCurve: { default: '' },
+            capPosition: { default: 'end' }
+        },
+
+
+        edit: ({ className, attributes, setAttributes }) => {
+
+            return (
+                el('div', {
+                    style: {
+                        height: '100%'
+                    }
+                }, [
+                    el(InspectorControls, {}, [
+                        el(TextControl, {
+                            value: attributes.from,
+                            label: 'From',
+                            onChange: value => {
+                                setAttributes({ arrowFrom: value })
+                            }
+                        }),
+                        el(TextControl, {
+                            value: attributes.slope,
+                            label: 'Slope',
+                            onChange: value => {
+                                setAttributes({ arrowSlope: value })
+                            }
+                        }),
+                        el(TextControl, {
+                            value: attributes.curve,
+                            label: 'Curve',
+                            onChange: value => {
+                                setAttributes({ arrowCurve: value })
+                            }
+                        }),
+                        el(TextControl, {
+                            value: attributes.capPosition,
+                            label: 'Cap position',
+                            onChange: value => {
+                                setAttributes({ capPosition: value })
+                            }
+                        }),
+                        el(RangeControl, {
+                            value: attributes.wrapperTop,
+                            label: 'Move vertically',
+                            min: -100,
+                            max: 100,
+                            onChange: value => {
+                                setAttributes({ wrapperTop: value })
+                            }
+                        }),
+                        el(RangeControl, {
+                            value: attributes.wrapperLeft,
+                            label: 'Move horizontally',
+                            min: -100,
+                            max: 100,
+                            onChange: value => {
+                                setAttributes({ wrapperLeft: value })
+                            }
+                        }),
+                        el(RangeControl, {
+                            value: attributes.wrapperHeight,
+                            label: 'Scale vertically',
+                            min: 0,
+                            max: 200,
+                            step: 1,
+                            onChange: value => setAttributes({ wrapperHeight: value })
+                        }),
+                        el(RangeControl, {
+                            value: attributes.wrapperWidth,
+                            label: 'Scale horizontally',
+                            min: 0,
+                            max: 200,
+                            step: 1,
+                            onChange: value => setAttributes({ wrapperWidth: value })
+                        }),
+                        el(RangeControl, {
+                            value: attributes.capSize,
+                            label: 'Cap size',
+                            min: 0,
+                            max: 50,
+                            step: 0.5,
+                            onChange: value => setAttributes({ capSize: value })
+                        }),
+                        el(RangeControl, {
+                            value: attributes.stroke,
+                            label: 'Stroke',
+                            min: 0,
+                            max: 2,
+                            step: 0.05,
+                            onChange: value => setAttributes({ stroke: value })
+                        })
+                    ]),
+                    renderArrow(attributes)
+                ])
+            )
+        },
+
+        save: ({ attributes }) => renderArrow(attributes)
+    })
+
+})()
+
+
+
+
---
+++ wordpress-block/arrows.php
@@ -0,0 +1,47 @@
+<?php
+add_action( 'init', 'djmarian_register_block_arrows');
+function djmarian_register_block_arrows() {
+    if ( ! is_gutenberg_active() ) {
+        return;
+    }
+
+    $block_name = 'arrows';
+
+    wp_register_script( 
+        $block_name, 
+        get_theme_file_uri() . '/blocks/arrows/wordpress-block/arrows.js', 
+        [
+            'wp-blocks',
+            'wp-i18n',
+            'wp-element',
+            'wp-components',
+            'wp-editor'
+        ]
+    );
+
+    wp_register_style( 
+        $block_name,
+        get_theme_file_uri() . '/blocks/arrows/css/main.css', 
+        [ 
+            'wp-edit-blocks' 
+        ]
+    );
+
+    register_block_type( 'djmarian/arrows', [
+        'editor_script' => $block_name,
+        'editor_style' => $block_name,
+        'style' => $block_name
+    ]);
+
+}
+
+if (! function_exists('is_gutenberg_active')){
+    function is_gutenberg_active(){
+        return function_exists('register_block_type');
+    }
+}
+
+function test_djmarian_register_block_arrows(){
+    echo 'Directorio: ' . get_theme_file_uri();
+}
+
